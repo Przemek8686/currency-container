b2aececcb063f84f731ab14ed4863242aad9f4ba
Przemek Krawczynski <1986krawiec1986@gmail.com>
2023-01-22 19:16:17 +0100

    function added

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,7 @@
     const headingElement = document.querySelector(".js-heading");
 
 
+    
     const onFormSubmit = (event) => {
         event.preventDefault();
 
@@ -16,14 +17,14 @@
         const euroElement = document.querySelector(".js-euro");
 
 
-        const euro = euroElement.value;
-        const pln = plnElement.value;
 
         const result = euro * pln;
         resultElement.innerText = result.toFixed(2);
 
     };
-
+const CalculateResult =(euro,pln) =>{
+        const euro = euroElement.value;
+        const pln = plnElement.value;}
     const init = () => {
 
         headingElement.addEventListener("submit", onFormSubmit);
